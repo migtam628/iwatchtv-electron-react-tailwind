a5eb99741455b3a1fe05d9146371905e2bc6c67c
Mig Tam Rod <migtam628@gmail.com>
2022-04-03 21:59:59 -0400

    Initial commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,27 @@
+import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+import AppProvider from './providers/app';
+
+const Hello = () => {
+  return (
+    <div className="text-center">
+      <img src={icon} alt="logo" className="h-48 w-48" />
+      <h1 className="text-2xl font-bold">
+        electron-react-boilerplate with tailwind
+      </h1>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <AppProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hello />} />
+        </Routes>
+      </Router>
+    </AppProvider>
+  );
+}
